01aaba6b5e4ae8a59e8693c6e9098ffca25ee3e5
dngtundt <dtun11111@gmail.com>
2024-11-02 23:26:55 +0700

    Team 2 homework Anh Tuan 8 + 9

---
+++ first_week/Team2/Homework/AnhTuan/lesson1_8.py
@@ -0,0 +1,19 @@
+﻿'''8. Những năm nào chia hết cho 4 được và không chia hết cho 100 được coi là năm nhuận (ví dụ năm 
+2100 không phải là năm nhuận, 2104 là năm nhuận). Viết chương trình hỏi người dùng một năm và 
+in ra xem đó có phải là năm nhuận hay không? '''
+
+def is_leap_year(year):
+    if (year % 4 == 0) and (year % 100 != 0):
+        return True
+    elif year % 400 == 0:
+        return True
+    else:
+        return False
+
+year = int(input("Nhap nam: "))
+
+if is_leap_year(year):
+    print(f"{year} la nam nhuan")
+else:
+    print(f"{year} khong la nam nhuan")
+
---
+++ first_week/Team2/Homework/AnhTuan/lesson1_9.py
@@ -0,0 +1,31 @@
+﻿# Hinh chu nhat day
+long = int(input("Nhap chieu dai: "))
+width = int(input("Nhap chieu rong: "))
+
+for i in range(width):
+    print('* ' * long)
+
+# Hinh chu nhat rong
+long = int(input("Nhap chieu dai: "))
+width = int(input("Nhap chieu rong: "))
+
+for i in range(width):
+    if i == 0 or i == width - 1:
+        print('* ' * long)
+    else:
+        print('* ' + '  ' * (long - 2) + '*')
+
+# Nua phai hinh tam giac
+height = int(input("Nhap chieu cao cua tam giac: "))
+
+for i in range(1, height + 1): # Mac dinh cua end là n - 1 nhung vi start tai 1 (neu start = 0 thi se tao ra khoang trong vo nghia) nen end phai + 1 de can bang voi start
+    print('*' * i) # 'x ' se nhin thay ro hon bang mat thuong la tg vuong can x = y = i
+
+# Hinh thoi
+height = int(input("Nhap chieu cao cua hinh thoi: "))
+for i in range(1, height + 1):
+    print(' ' * (height - i) + '*' * (2 * i - 1))
+for i in range(height - 1, 0, -1): # Start height - 1 de tranh bi trung lap voi for cua tg dau tien
+    print(' ' * (height - i) + '*' * (2 * i - 1)) # For nguoc de xuat tg con lai
+
+
