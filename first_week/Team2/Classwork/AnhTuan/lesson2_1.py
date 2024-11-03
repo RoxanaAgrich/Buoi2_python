f93b9c72092e22178ec14a4e0a9c623e53fba857
dngtundt <dtun11111@gmail.com>
2024-11-03 21:30:40 +0700

    Team 2 classwork AnhTuan 1 + 2

---
+++ first_week/Team2/Classwork/AnhTuan/lesson2_1.py
@@ -0,0 +1,21 @@
+﻿''' Viết hàm tạo mảng số nguyên a với n phần tử và mảng b chỉ chứa các phần tử chẵn của a.'''
+
+import random
+
+def tao_mang_a(n, min_val=1, max_val=100):
+    # Tao random a
+    a = [random.randint(min_val, max_val) for _ in range(n)]
+    return a
+
+def tao_mang_b(a):
+    # Tao b la tap hop con cua a
+    b = [x for x in a if x % 2 == 0]
+    return b
+
+n = int(input("Nhap so pt mang a: "))
+a = tao_mang_a(n)
+b = tao_mang_b(a)
+
+print("Mang a:", a)
+print("Mang b (pt chan):", b)
+
---
+++ first_week/Team2/Classwork/AnhTuan/lesson2_2.py
@@ -0,0 +1,31 @@
+﻿''' Viết hàm trộn 2 mảng một chiều thành 1 mảng một chiều với mỗi phần tử của mảng mới 
+là tổng của 2 phần tương ứng từ 2 mảng cho trước. Trong quá trình trộn 2 mảng nếu mảng 
+nào còn phần tử thì các phần tử còn lại của mảng đó sẽ đưa vào mảng mới. 
+Ví dụ: 
+Mảng a: 3 9 1 4 
+Mảng b: 2 7 4 3 2 8 
+Mảng kết quả: 5 16 5 7 2 8. '''
+
+def tron_mang(a, b):
+    max_len = max(len(a), len(b))
+    ket_qua = []
+    
+    for i in range(max_len):
+        # ktra ptu o vtri i co thuoc a hay ko
+        phan_tu_a = a[i] if i < len(a) else 0
+        # ktra ptu o vtri i co thuoc b hay ko
+        phan_tu_b = b[i] if i < len(b) else 0
+        
+        # Them tong cua 2 pt vao mang kq
+        ket_qua.append(phan_tu_a + phan_tu_b)
+    
+    return ket_qua
+
+a = [3, 9, 1, 4]
+b = [2, 7, 4, 3, 2, 8]
+
+ket_qua = tron_mang(a, b)
+print("Mảng a:", a)
+print("Mảng b:", b)
+print("Mảng kết quả:", ket_qua)
+
