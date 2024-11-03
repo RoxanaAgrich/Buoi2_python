1f23040fb3e9e64a46b529f33adc8f2f8a6a47cb
TrHuy-0000 <166635906+TrHuy-0000@users.noreply.github.com>
2024-11-03 22:12:52 +0700

    Add files via upload

---
+++ first_week/Team2/Homework/AnhTuan/Tuan1_btvn_b3.py
@@ -0,0 +1,15 @@
+#Tính tích các số lẻ (tich = 1x3x5x…x n)
+import math
+#Cách 1 
+tong = 1
+a = int(input("Nhap so: ")) #Nhap so n
+for sum_odd in range(1, a+1, 2): # Chay tu 1 đến n, cách nhau 1 số
+        tong *= sum_odd # Tính tổng nhân các số chẵn
+print("Nhan cac so chan: ", tong) # Xuất tổng
+
+#Cách 2
+result = 1
+b = int(input("Nhap so: ")) 
+result = range(1, b+1, 2) # Biến lưu giữ giá trị sau khi chạy range
+s = math.prod(result) # Tính tổng nhân các số chẵn
+print("Nhan cac so chan: ", tong) # Xuất tổng
